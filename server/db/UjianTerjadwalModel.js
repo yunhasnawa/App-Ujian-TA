ae87d47df06b6e0adb125bf07010f5351c00cd7c
ilzam <ilzammulkhaq85@gmail.com>
2022-12-12 10:54:52 +0700

    fix: base CRUD again

--- server/db/UjianTerjadwalModel.js
+++ server/db/UjianTerjadwalModel.js
@@ -3,44 +3,47 @@ const { ObjectId } = require("mongodb");
 
 const COLLECTION_NAME = process.env.MONGO_COLLECTION_NAME;
 
-class UjianTerjadwalModel {
-
-    async findAll() {
-        let data = await db().collection(COLLECTION_NAME).find().toArray();
-        return data;
-    }
-
-    async findOneById(id) {
-        let data = await db().collection(COLLECTION_NAME).findOne({ _id: ObjectId(id) });
-        return data;
-    }
-
-    async addNew(obj, callback) {
-        let data = await db().collection(COLLECTION_NAME).insertOne(obj, callback);
-        return data;
-    }
-
-    async edit(id, updatedObj, callback) {
-        let query = { _id: ObjectId(id) };
-
-        let newValues = {
-            $set: {}
-        };
-
-        for (const [key, value] of Object.entries(updatedObj)) {
-            newValues['$set'][key] = value;
-        };
-
-        let data = await db().collection(COLLECTION_NAME).updateOne(query, newValues);
-
-        return data;
-    }
-
-    async deleteOneById(id, callback) {
-        let query = { _id: ObjectId(id) };
-        let data = await db().collection(COLLECTION_NAME).remove(query);
-        return data;
-    }
+const findAll = async () => {
+    let data = await db().collection(COLLECTION_NAME).find().toArray();
+    return data;
 }
 
-module.exports = new UjianTerjadwalModel();
+const findOneById = async (id) => {
+    let data = await db().collection(COLLECTION_NAME).findOne({ _id: ObjectId(id) });
+    return data;
+}
+
+const addNew = async (obj) => {
+    let data = await db().collection(COLLECTION_NAME).insertOne(obj);
+    return data;
+}
+
+const edit = async (id, updatedObj) => {
+    let query = { _id: ObjectId(id) };
+
+    let newValues = {
+        $set: {}
+    };
+
+    for (const [key, value] of Object.entries(updatedObj)) {
+        newValues['$set'][key] = value;
+    };
+
+    let data = await db().collection(COLLECTION_NAME).updateOne(query, newValues);
+
+    return data;
+}
+
+const deleteOneById = async (id) => {
+    let query = { _id: ObjectId(id) };
+    let data = await db().collection(COLLECTION_NAME).remove(query);
+    return data;
+}
+
+module.exports = {
+    findAll,
+    findOneById,
+    addNew,
+    edit,
+    deleteOneById
+};
--- server/routes/record.js
+++ server/routes/record.js
@@ -3,32 +3,32 @@ const Util = require("../lib/Util");
 
 const recordRoutes = express.Router();
 
-const model = require("../db/UjianTerjadwalModel");
+const { findAll, findOneById, addNew, deleteOneById, edit } = require("../db/UjianTerjadwalModel");
 
 recordRoutes.route("/record").get(async (req, res) => {
-    let data = await model.findAll();
+    let data = await findAll();
     res.json(data);
 });
 
 recordRoutes.route("/record/:id").get(async (req, res) => {
-    let data = await model.findOneById(req.params.id);
+    let data = await findOneById(req.params.id);
     res.json(data);
 });
 
 recordRoutes.route("/record/add").post(async (req, res) => {
     let obj = Util.reqToObj(req);
-    let data = await model.addNew(obj);
+    let data = await addNew(obj);
     res.json(data);
 });
 
 recordRoutes.route("/update/:id").put(async (req, res) => {
     let updatedObj = Util.reqToObj(req);
-    let data = await model.edit(req.params.id, updatedObj);
+    let data = await edit(req.params.id, updatedObj);
     res.json(data);
 });
 
 recordRoutes.route("/delete/:id").delete(async (req, res) => {
-    let data = await model.deleteOneById(req.params.id)
+    let data = await deleteOneById(req.params.id)
     res.json(data);
 });
 
